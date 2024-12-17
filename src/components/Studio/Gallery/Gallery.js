8e33450eeb1fe3f8b91b9cb0ab32f50bde231337
Vgrotherz <vgrotherz@gmail.com>
2024-12-17 23:03:25 +0700

    размер фото уменьшен

--- src/components/Studio/Gallery/Gallery.js
+++ src/components/Studio/Gallery/Gallery.js
@@ -3,24 +3,24 @@ import './gallery.scss'
 
 const photoGallBig = [
   { original: require('../../../media/big/1.jpg') },
-  { original: require('../../../media/big/2.JPG') },
-  { original: require('../../../media/big/3.JPG') },
-  { original: require('../../../media/big/4.JPG') },
-  { original: require('../../../media/big/5.JPG') },
-  { original: require('../../../media/big/6.JPG') },
-  { original: require('../../../media/big/7.JPG') },
-  { original: require('../../../media/big/8.JPG') },
+  { original: require('../../../media/big/2.jpg') },
+  { original: require('../../../media/big/3.jpg') },
+  { original: require('../../../media/big/4.jpg') },
+  { original: require('../../../media/big/5.jpg') },
+  { original: require('../../../media/big/6.jpg') },
+  { original: require('../../../media/big/7.jpg') },
+  { original: require('../../../media/big/8.jpg') },
 ];
 
 const photoGallSmall = [
-   { original: require('../../../media/small/1.jpeg') },
-   { original: require('../../../media/small/2.jpeg') },
-   { original: require('../../../media/small/3.jpeg') },
-   { original: require('../../../media/small/4.jpeg') },
-   { original: require('../../../media/small/5.jpeg') },
-   { original: require('../../../media/small/6.jpeg') },
-   { original: require('../../../media/small/7.jpeg') },
-   { original: require('../../../media/small/8.jpeg') },
+   { original: require('../../../media/small/1.jpg') },
+   { original: require('../../../media/small/2.jpg') },
+   { original: require('../../../media/small/3.jpg') },
+   { original: require('../../../media/small/4.jpg') },
+   { original: require('../../../media/small/5.jpg') },
+   { original: require('../../../media/small/6.jpg') },
+   { original: require('../../../media/small/7.jpg') },
+   { original: require('../../../media/small/8.jpg') },
 ]
 
 const galleryProps = {
